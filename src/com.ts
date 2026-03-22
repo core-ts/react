1a19d60176ce4fbe0b1b351dc0b39d44637acaba
Duc Nguyen <minhduc140583@gmail.com>
2026-03-22 11:43:48 +0700

    Rename btnBack to backBtn

--- src/com.ts
+++ src/com.ts
@@ -13,7 +13,7 @@ export function Error(p: ErrorProps) {
     React.createElement(
       "header",
       {},
-      p.back && React.createElement("button", { type: "button", id: "btnBack", name: "btnBack", className: "btn-back", onClick: p.back }),
+      p.back && React.createElement("button", { type: "button", id: "backBtn", name: "backBtn", className: "btn-back", onClick: p.back }),
       React.createElement("h2", {}, p.title),
     ),
     React.createElement("div", { className: "error-body" }, React.createElement("h4", { className: "h4" }, p.message)),
@@ -21,7 +21,7 @@ export function Error(p: ErrorProps) {
   /*
   <div>
     <header>
-      <button type="button" id="btnBack" name="btnBack" className="btn-back" onClick={() => navigate(-1)}></button>
+      <button type="button" id="backBtn" name="backBtn" className="btn-back" onClick={() => navigate(-1)}></button>
       <h2>{resource.error_404_title}</h2>
     </header>
     <div className="error-body">
@@ -59,39 +59,33 @@ export interface Props {
   onChange?: (e: any, callback?: (() => void) | undefined, lc?: Locale | undefined) => void
 }
 export function Search(p: Props) {
-  return React.createElement(
-    React.Fragment,
-    null,
-    React.createElement(
-      "label",
-      { className: p.className },
-      p.pageSizeChanged && React.createElement(PageSizeSelect, { size: p.size, sizes: p.sizes, onChange: p.pageSizeChanged, name: p.name, id: p.id }),
-      React.createElement("input", {
-        type: "text",
-        id: "q",
-        name: "q",
-        value: p.value || "",
-        onChange: p.onChange,
-        maxLength: p.maxLength,
-        placeholder: p.placeholder,
-      }),
-      p.clear &&
-        React.createElement("button", { type: "button", id: "btnClearQ", name: "btnClearQ", hidden: !p.value, className: "btn-remove-text", onClick: p.clear }),
-      p.toggle && React.createElement("button", { type: "button", id: "btnToggleSearch", name: "btnToggleSearch", className: "btn-filter", onClick: p.toggle }),
-      p.search && React.createElement("button", { type: "submit", id: "btnSearch", name: "btnSearch", className: "btn-search", onClick: p.search }),
-    ),
+  React.createElement(
+    "label",
+    { className: p.className },
+    p.pageSizeChanged && React.createElement(PageSizeSelect, { size: p.size, sizes: p.sizes, onChange: p.pageSizeChanged, name: p.name, id: p.id }),
+    React.createElement("input", {
+      type: "text",
+      id: "q",
+      name: "q",
+      value: p.value || "",
+      onChange: p.onChange,
+      maxLength: p.maxLength,
+      placeholder: p.placeholder,
+    }),
+    p.clear &&
+      React.createElement("button", { type: "button", id: "clearQBtn", name: "clearQBtn", hidden: !p.value, className: "btn-remove-text", onClick: p.clear }),
+    p.toggle && React.createElement("button", { type: "button", id: "toggleSearchBtn", name: "toggleSearchBtn", className: "btn-filter", onClick: p.toggle }),
+    p.search && React.createElement("button", { type: "submit", id: "searchBtn", name: "searchBtn", className: "btn-search", onClick: p.search }),
   )
   /*
   return (
-    <>
-      <label className={p.className}>
-        {p.pageSizeChanged && <PageSizeSelect size={p.size} sizes={p.sizes} onChange={p.pageSizeChanged} name={p.name} id={p.id} />}
-        <input type='text' id='q' name='q' value={p.value || ''} onChange={p.onChange} maxLength={p.maxLength} placeholder={p.placeholder} />
-        {p.clear && <button type='button' id='btnClearQ' name='btnClearQ' hidden={!p.value} className='btn-remove-text' onClick={p.clear}/>}
-        {p.toggle && <button type='button' id='btnToggleSearch' name='btnToggleSearch' className='btn-filter' onClick={p.toggle} />}
-        {p.search && <button type='submit' id='btnSearch' name='btnSearch' className='btn-search' onClick={p.search} />}
-      </label>
-    </>
+    <label className={p.className}>
+      {p.pageSizeChanged && <PageSizeSelect size={p.size} sizes={p.sizes} onChange={p.pageSizeChanged} name={p.name} id={p.id} />}
+      <input type='text' id='q' name='q' value={p.value || ''} onChange={p.onChange} maxLength={p.maxLength} placeholder={p.placeholder} />
+      {p.clear && <button type='button' id='clearQBtn' name='clearQBtn' hidden={!p.value} className='btn-remove-text' onClick={p.clear}/>}
+      {p.toggle && <button type='button' id='toggleSearchBtn' name='toggleSearchBtn' className='btn-filter' onClick={p.toggle} />}
+      {p.search && <button type='submit' id='searchBtn' name='searchBtn' className='btn-search' onClick={p.search} />}
+    </label>
   );
   */
 }
